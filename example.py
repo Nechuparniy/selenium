2500a7304bcb093469ead04e27dccc59f252d6a7
Nechuparniy <nechuparniy@gmail.com>
2020-12-17 13:43:25 +0300

    add lesson 2 task 3

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome() #Firefox(firefox_binary="c:\\Program Files (x86)\\Nightly\\firefox.exe")
-    print(wd.capabilities)
+    wd.implicitly_wait(10)
     request.addfinalizer(wd.quit)
     return wd
 
@@ -15,6 +15,13 @@ def driver(request):
 def test_example(driver):
     driver.get("http://www.google.com/")
     driver.find_element_by_name("q").send_keys("webdriver")
-    WebDriverWait(driver, 10).until(EC.visibility_of(driver.find_element_by_name("btnK")))
     driver.find_element_by_name("btnK").click()
     WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
+
+
+def test_lesson2_task3(driver):
+    driver.get('http://localhost/litecart/admin')
+    driver.find_element_by_name('username').send_keys('admin')
+    driver.find_element_by_name('password').send_keys('admin')
+    driver.find_element_by_name('login').click()
+    WebDriverWait(driver, 10).until(EC.title_is('My Store'))
