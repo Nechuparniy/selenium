9d6425b7c010d37b9cea61dc742c02f786ffacf9
Nechuparniy <nechuparniy@gmail.com>
2021-01-20 21:21:52 +0300

    add tas9

--- task8.py
+++ task8.py
@@ -1,9 +1,5 @@
 import pytest
-import time
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 
 @pytest.fixture
---
+++ task9.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    # wd = webdriver.Firefox(firefox_binary="C:\\Program Files\\Firefox Nightly\\firefox.exe")
+    wd.implicitly_wait(10)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def check_alphabetical_order(list1):
+    sorted_list = list(list1)
+    sorted_list.sort()
+    assert sorted_list == list1
+
+
+def test_task9(driver):
+    driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_css_selector('button[type=submit]').click()
+    rows = driver.find_elements_by_css_selector("tr.row")
+    countries_list = []
+    countries_with_zones = []
+    for x in rows:
+        country = x.find_element_by_css_selector("td a:not([title=Edit])").text
+        countries_list.append(country)
+        if x.find_element_by_css_selector("td:nth-child(6)").text != '0':
+            countries_with_zones.append(country)
+    check_alphabetical_order(countries_list)
+    for i in countries_with_zones:
+        zones = []
+        driver.find_element_by_xpath(f"//a[contains(text(), '{i}')]").click()
+        zones.append(driver.find_element_by_css_selector("table.dataTable tr td:nth-child(3)").text)
+        check_alphabetical_order(zones)
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
